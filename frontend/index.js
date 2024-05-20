345bb30d2bbaad92c33aec98c079f11a9a0d0c30
wprince0288 <wprince0288@gmail.com>
2024-05-19 19:46:07 -0500

    Task 2 complete

--- frontend/index.js
+++ frontend/index.js
@@ -12,9 +12,22 @@ function moduleProject1() {
   widgets.forEach(widget => {
     widget.classList.add('widget')
   })
-  
+
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
+const randomIdx = Math.floor(Math.random() * quotes.length)
+const randomQuote = quotes[randomIdx]
+const quote = document.createElement('div')
+const quoteText = randomQuote.quote
+quote.textContent = quoteText
+const widget1 = document.querySelector('.quoteoftheday')
+widget1.appendChild(quote)
+
+const authorDate = document.createElement('div')
+const { author, date } = randomQuote
+authorDate.textContent = `${author} in ${date || "an unknown date"}`
+widget1.appendChild(authorDate)
+
 
   // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
